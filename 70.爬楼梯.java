014020394836e905c9fd3f1984aebe1b2688bf9d
vic <>
2020-03-09 21:14:52 +0800

    Accepted 45/45 cases passed (0 ms) Your runtime beats 100 % of java submissions Your memory usage beats 5.18 % of java submissions (36.6 MB)

---
+++ 70.爬楼梯.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=java
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+class Solution {
+    //1:1  2:2  3:3  4:5 5:8   ...斐波那契数列
+
+    // 1 1
+    // 2
+
+    // 1 1 1
+    // 1 2
+    // 2 1
+
+    // 1 1 1 1
+    // 1 1 2
+    // 1 2 1
+    // 2 1 1
+    // 2 2
+
+
+    // 1 1 1 1 1
+    // 1 1 1 2
+    // 1 1 2 1
+    // 1 2 1 1
+    // 1 2 2
+    // 2 1 1 1
+    // 2 1 2
+    // 2 2 1
+    
+    public int climbStairs(int n) {
+        //用一个变量记录已经计算过的值，不会超时?
+        if(n <= 2) 
+            return n;
+        int first=1,second=2,current=0;
+        for(int i=3;i<=n;i++){
+            current = first + second;
+            first = second;
+            second = current;
+        }
+        return current;
+    }
+}
+// @lc code=end
+
